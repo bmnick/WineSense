e9b04d40f886e35f3e9621c372564e4aaf0036d4
Ben Nicholas <ben@ben-nicholas.com>
2011-01-31 18:46:13 -0500

    rails without prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineSense::Application.config.secret_token = 'f9e0ff0bb9cf91b8648f49900e91744871dc1d036741ecf7c1f138f3ebc411a7f1145aec7e32f96056a6b78759275e0ac6db6fb98f3908e8fcc4c8d49daf1c62'
