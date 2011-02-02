c6bcd86325edc548de925ad383a8d94f63f15edc
Chris Hossenlopp <chris@chrishossenlopp.com>
2011-02-01 21:28:31 -0500

    Added JSON for the view

---
+++ public/javascripts/aromaViewTemplate.js
@@ -0,0 +1,48 @@
+[
+	{
+		"L1Name" : "Vegetative",
+		"L2" : [
+			{
+				"L2Name" : "Fresh",
+				"L3" : [
+					{ "L3Name" : "Stemmy" },
+					{ "L3Name" : "Grass, Cut Green" },
+					{ "L3Name" : "Bell Pepper" },
+					{ "L3Name" : "Eucalyptus" },
+					{ "L3Name" : "Mint" }
+				]
+			},
+			{
+				"L2Name" : "Canned / Cooked",
+				"L3" : [
+					{ "L3Name" : "Green Beans" },
+					{ "L3Name" : "Asparagus" },
+					{ "L3Name" : "Green Olive" },
+					{ "L3Name" : "Black Olive" },
+					{ "L3Name" : "Artichoke" }
+				]
+			},
+			{
+				"L2Name" : "Dried",
+				"L3" : [
+					{ "L3Name" : "Hay / Straw" },
+					{ "L3Name" : "Tea" },
+					{ "L3Name" : "Tobacco" }
+				]
+			}
+		]
+	},
+	{
+		"L1Name" : "Nutty",
+		"L2" : [
+			{
+				"L2Name" : "Nutty",
+				"L3" : [
+					{ "L3Name" : "Walnut" },
+					{ "L3Name" : "Hazelnut" },
+					{ "L3Name" : "Almond" }
+				]
+			}
+		]
+	}
+]
