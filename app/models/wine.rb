151e412ddcec3fdbfffa3fd3f6b457e241d3802a
Ben Nicholas <ben@ben-nicholas.com>
2011-02-03 02:04:42 -0500

    Parse child json_strings and combine intelligently

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -8,4 +8,35 @@ class Wine < ActiveRecord::Base
   def to_s
     "#{year} #{winery} #{wine}"
   end
+  
+  # returns either the fully combined JSON string of all 
+  # tastings or nil, if no tastings have been recorded
+  def aggregate_tastings
+    unless self.tastings.size == 0
+      scents = parse_tastings
+      aggregate_json = combine_json scents
+    end
+    
+    aggregate_json ||= "{}"
+  end
+  
+  # returns an array of hashes representing each parsed json string
+  def parse_tastings
+    scents = Array.new
+    
+    self.tastings.each do |tasting|
+      scents << JSON.parse(tasting.json_string)
+    end
+    
+    scents
+  end
+  
+  # returns a combined json string of all hashes
+  def combine_json scents
+    full_json = scents.inject(Hash.new) do |result, cur|
+      result.update(cur) {|key, running, cur| running + cur }
+    end
+    
+    full_json.to_json
+  end
 end
